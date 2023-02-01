b49f50da4ba1ad40b4f7d9bc45fa7165733418d2
palisjam <pluckananuru@iom.int>
2023-02-01 10:36:22 +0700

    MRZ date validator dll
    You can use this dll as a reference in your project and then initialize the class ValidateDate(int maxPassportValidity). the class has 2 methods:
    1. DoB = date of birth, if the date of birth is future (compare with today's date), it will automatically change to 19XX
    2. DoE = date of expiry, if the date of expiry is more than the current year +10 (maxPassportValidity, it will change to 19XX

---
+++ MrzDateValidator/MrzDateValidator.cs
@@ -0,0 +1,50 @@
+﻿// This Class dll contributed by Palis Luckananurug
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrzDateValidator
+{
+    public class ValidateDate
+    {
+
+        private int MaxPassportValidity;
+        public ValidateDate(int maxPassportValidity) {
+            MaxPassportValidity = maxPassportValidity; 
+        }
+
+        static DateTime parseDate(string dateMRZ, string format)
+        {
+            DateTime convertedDate;
+            DateTime.TryParseExact(dateMRZ, format, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out convertedDate);
+            return convertedDate;
+        }
+
+        public DateTime DoB(string dateMRZ) //if date of birth is future, it will automatically change to 19XX
+        {
+            DateTime dateOfBirth = parseDate(dateMRZ, "yyMMdd");
+            DateTime currentDate = DateTime.Now;
+            if (currentDate.Date < dateOfBirth.Date)
+            {
+                dateOfBirth = parseDate("19" + dateMRZ, "yyyyMMdd");
+            }
+            return dateOfBirth;
+        }
+
+        public DateTime DoE(string dateMRZ) //if date of expiry is more than current year +10, it will change to 19XX
+        {
+            DateTime dateOfExpiry = parseDate(dateMRZ, "yyMMdd");
+            DateTime currentDate = DateTime.Now;
+            DateTime tenYearsLater = currentDate.AddYears(MaxPassportValidity); //current date +MaxPassport Validity (10)
+            if (tenYearsLater.Date < dateOfExpiry.Date)
+            {
+                dateOfExpiry = parseDate("19" + dateMRZ, "yyyyMMdd");
+            }
+            return dateOfExpiry;
+        }
+    }
+}
