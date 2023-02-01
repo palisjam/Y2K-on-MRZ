20e0e3be6e69f1896f610afbd67e86bbc65fb690
palisjam <pluckananuru@iom.int>
2023-02-01 14:32:45 +0700

    run result from 99 to 00

--- Program.cs
+++ Program.cs
@@ -16,11 +16,24 @@ namespace Y2K
         {
             ValidateDate _validateDate = new ValidateDate();
 
+            /*
             string date;
             date = Console.ReadLine();
             Console.WriteLine("MRZ date:" + date);
             Console.WriteLine("Date of Birth:" + _validateDate.DoB(date).ToString("dd-MMM-yyyy"));
             Console.WriteLine("Date of Expiry:" + _validateDate.DoE(date).ToString("dd-MMM-yyyy"));
+            Console.ReadLine();
+            */
+
+            string date;
+            for (int i = 99; i >= 0; i--)
+            {
+                string result = i < 10 ? "0" + i.ToString() : i.ToString();
+                date = result + "0101";
+                Console.WriteLine("MRZ date:" + date);
+                Console.WriteLine("Date of Birth:" + _validateDate.DoB(date).ToString("dd-MMM-yyyy"));
+                Console.WriteLine("Date of Expiry:" + _validateDate.DoE(date).ToString("dd-MMM-yyyy")+"\n");
+            }
             Console.ReadLine();
         }
     }
